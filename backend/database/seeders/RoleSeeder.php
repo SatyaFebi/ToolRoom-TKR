b662c242e938c5cebb0e810457927971753a81e9
Satya Febi N <satyafebin@gmail.com>
2025-08-07 10:29:17 +0700

    first project initialization

---
+++ backend/database/seeders/RoleSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'create job',
+            'edit job',
+            'delete job',
+            'view job',
+            'view report'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'api']);
+        }
+
+        $role = Role::create(['name' => 'kepala kompetensi', 'guard_name' => 'api']);
+        $role->givePermissionTo(['create job', 'edit job', 'delete job', 'view job', 'view report']);
+
+        $role = Role::create(['name' => 'guru', 'guard_name' => 'api']);
+        $role->givePermissionTo(['create job', 'edit job', 'delete job', 'view job', 'view report']);
+
+        $role = Role::create(['name' => 'murid', 'guard_name' => 'api']);
+        $role->givePermissionTo(['view job']);
+    }
+}
